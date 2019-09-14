570bb3f763fe458ba0901bda55a7082b2e423230
Thomas White <thomas.white.ucf@gmail.com>
2019-09-14 04:12:26 -0400

    removed file path for heroku

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -16,16 +16,19 @@ module.exports = function(app) {
 
 	// * A GET Route to `/survey` which should display the survey page.
 	app.get('/survey', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/survey.html'));
+		res.sendFile(path.join(__dirname, 'public/survey.html'));
+		// removed ../ from path
 	});
 
 	// * A default, catch-all route that leads to `home.html` which displays the home page.
 	app.get('/', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/home.html'));
+		res.sendFile(path.join(__dirname, 'public/home.html'));
+		// removed ../ from path
 	});
 
 	// * A default, catch-all route that leads to `home.html` which displays the home page.
 	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/home.html'));
+		res.sendFile(path.join(__dirname, 'public/home.html'));
+		// removed ../ from path
 	});
 };
