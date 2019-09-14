8620dd1cb1b20634137086dfa177c0714b3f0109
Thomas White <thomas.white.ucf@gmail.com>
2019-09-14 04:00:44 -0400

    heroku

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -20,7 +20,7 @@ module.exports = function(app) {
 	});
 
 	// * A default, catch-all route that leads to `home.html` which displays the home page.
-	app.get("/", function(req, res) {
+	app.get('/', function(req, res) {
 		res.sendFile(path.join(__dirname, '../public/home.html'));
 	});
 
