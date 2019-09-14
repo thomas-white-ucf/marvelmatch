edaab0342989913453715fa6700597ef2a9c9827
Thomas White <thomas.white.ucf@gmail.com>
2019-09-14 02:13:57 -0400

    User can submit info, saves to data file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,65 +1,94 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
+// LOAD DATA - link routes to a series of "data" sources, such as current friends list, or marvel characters list
 var friends = require('../data/friends');
 
-module.exports = function(app) {
-	// API GET Requests
-	// Below code handles when users "visit" a page.
-	// In each of the below cases when a user visits a link
-	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-	// ---------------------------------------------------------------------------
+// ROUTES with express
 
+module.exports = function(app) {
+	//
+	// ** A GET route with the url `/api/friends`.
+	// ** This will be used to display a JSON of all possible friends.
 	app.get('/api/friends', function(req, res) {
-        res.json(friends);
+		res.json(friends);
 	});
 
-	// API POST Requests
-	// Below code handles when a user submits a form and thus submits data to the server.
-	// In each of the below cases, when a user submits form data (a JSON object)
-	// ...the JSON is pushed to the appropriate JavaScript array
-	// (ex. User fills out a reservation request... this data is then sent to the server...
-	// Then the server saves the data to the tableData array)
-	// ---------------------------------------------------------------------------
-
+	//
+	// ** A POST routes `/api/friends`.
+	// ** This will be used to handle incoming survey results.
+	// ** This route will also be used to handle the compatibility logic.
 	app.post('/api/friends', function(req, res) {
-		// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-		// It will do this by sending out the value "true" have a table
-		// req.body is available since we're using the body parsing middleware
-
 		// ADD CODE TO COMPARE USER INPUT SCORE TO the array of freinds
+
+		// .. loop through friends, compare each i for req.body (SURVEY RESULT SUBMISSION) submission compare to each friend.
+		// Absolute value difference, save each, replace if it is lower, save that friends place in variable, display this in modal pop up
+
 		if (friends.length < 20) {
 			friends.push(req.body);
 			res.json(true);
+			console.log(friends);
 		} else {
 			res.json(false);
+			console.log(friends);
 		}
 	});
-
-	// ---------------------------------------------------------------------------
-	// I added this below code so you could clear out the table while working with the functionality.
-	// Don"t worry about it!
-
-	// app.post("/api/clear", function(req, res) {
-	//   // Empty out the arrays of data
-	//   tableData.length = 0;
-	//   waitListData.length = 0;
-
-	//   res.json({ ok: true });
-	// });
 };
 
+// =======
+// =======
+// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+// It will do this by sending out the value "true" have a table
+// req.body is available since we're using the body parsing middleware
+
+// Determine the user's most compatible friend using the following as a guide:
+//    	* Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+
+//    	* With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
+//      * Example: * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]` * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]` * Total Difference: **2 + 1 + 2 =** **_5_**
+//    	* Remember to use the absolute value of the differences.
+// 				Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
+//    	* The closest match will be the user with the least amount of difference.
+
+// Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+//    	* The modal should display both the name and picture of the closest match.
+// =======
+// =======
+
+// ===============================================================================
+//    Solved Hot Restaurant
+
+// API GET Requests
+// Below code handles when users "visit" a page.
+// In each of the below cases when a user visits a link
+// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+// ---------------------------------------------------------------------------
+
+// API POST Requests
+// Below code handles when a user submits a form and thus submits data to the server.
+// In each of the below cases, when a user submits form data (a JSON object)
+// ...the JSON is pushed to the appropriate JavaScript array
+// (ex. User fills out a reservation request... this data is then sent to the server...
+// Then the server saves the data to the tableData array)
+// ---------------------------------------------------------------------------
+
 // ===================================================================
 // ===================================================================
 
-// Your `apiRoutes.js` file should contain two routes:
+//  (Express SendFile - GET request from server to HTML views)
+//  (Jquery AJAX (get/POST))  server to HTML views
 
-//    * A GET route with the url `/api/friends`.               (Express SendFile - GET request from server to HTML views)
-// This will be used to display a JSON of all possible friends.
-//    * A POST routes `/api/friends`.                          (Jquery AJAX (get/POST))  server to HTML views
-//  This will be used to handle incoming survey results.
+// ---------------------------------------------------------------------------
+// I added this below code so you could clear out the table while working with the functionality.
+// Don"t worry about it!
 
-//  This route will also be used to handle the compatibility logic.
+// app.post("/api/clear", function(req, res) {
+//   // Empty out the arrays of data
+//   tableData.length = 0;
+//   waitListData.length = 0;
+
+//   res.json({ ok: true });
+// });
+
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, FRIENDS etc.
+// ===============================================================================
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,3 @@
-// Your `apiRoutes.js` file should contain two routes:
-
-//    * A GET route with the url `/api/friends`.
-// This will be used to display a JSON of all possible friends.
-//    * A POST routes `/api/friends`.
-// This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-
 // ===============================================================================
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
@@ -15,18 +8,18 @@ var path = require('path');
 module.exports = function(app) {
 	// HTML GET Requests
 	// Below code handles when users "visit" a page.
+
 	// In each of the below cases the user is shown an HTML page of content
 	// ---------------------------------------------------------------------------
 
+	// Use path dependency to get user directory name = __dirname
+
+	// * A GET Route to `/survey` which should display the survey page.
 	app.get('/survey', function(req, res) {
 		res.sendFile(path.join(__dirname, '../public/survey.html'));
 	});
 
-	app.get('/home', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/home.html'));
-	});
-
-	// If no matching route is found default to home
+	// * A default, catch-all route that leads to `home.html` which displays the home page.
 	app.get('*', function(req, res) {
 		res.sendFile(path.join(__dirname, '../public/home.html'));
 	});
