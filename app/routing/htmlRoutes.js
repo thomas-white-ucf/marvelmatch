c6ccba0fabfd12a242fae2cdf033d735604967a9
Thomas White <thomas.white.ucf@gmail.com>
2019-09-14 03:52:18 -0400

    added home route to test heroku

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -20,6 +20,11 @@ module.exports = function(app) {
 	});
 
 	// * A default, catch-all route that leads to `home.html` which displays the home page.
+	app.get("/", function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/home.html'));
+	});
+
+	// * A default, catch-all route that leads to `home.html` which displays the home page.
 	app.get('*', function(req, res) {
 		res.sendFile(path.join(__dirname, '../public/home.html'));
 	});
